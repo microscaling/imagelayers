25bf64fe2e8dfd0ac9457c2038b24ed8fb8d00b8
Laura Frank <ljfrank@gmail.com>
2015-02-16 16:34:25 -0500

    Updating Status method test

--- api/registry_test.go
+++ api/registry_test.go
@@ -1,12 +1,12 @@
 package api
 
 import (
-	"encoding/json"
-	"testing"
-
-	"github.com/CenturyLinkLabs/docker-reg-client/registry"
+//	"encoding/json"
+//	"testing"
+//
+//	"github.com/CenturyLinkLabs/docker-reg-client/registry"
 	"github.com/stretchr/testify/mock"
-	"github.com/stretchr/testify/assert"
+//	"github.com/stretchr/testify/assert"
 )
 
 type mockRegistryMgr struct {
