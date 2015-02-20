6a80757adfb1f820755406cf8191b1c876312176
Gary Paige <gary.paige@savvis.com>
2015-02-20 08:55:53 -0600

    services working now

--- graph/assets/scripts/services.js
+++ graph/assets/scripts/services.js
@@ -1,16 +1,12 @@
 'use strict';
 
-angular.module('iLayers')
-  .factory('registryService', ['$http',
-    function($http) {
+angular.module ('iLayers')
+  .factory ('registryService', ['$http',
+    function ($http) {
 
       return {
-          inspect: function() {
-            return $http.post("/registry/analyze", { "repos": [{"name": "centurylink/image-graph", "tag": "latest"}] })
-                        .then(function(response) {
-                           console.log(response);
-                           return { "layers": response.data }
-                        });
+          inspect: function () {
+            return $http.post("/registry/analyze", { "repos": [{"name": "centurylink/image-graph", "tag": "latest"}] });
           }
       };
   }]);
