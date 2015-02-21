3f57ce22dfb6804dd1ccd97b83711021ab70bb6c
Laura Frank <ljfrank@gmail.com>
2015-02-21 15:15:28 -0600

    Tree layout

--- graph/assets/scripts/filters.js
+++ graph/assets/scripts/filters.js
@@ -5,7 +5,7 @@ angular.module ('iLayers')
     var bytesToSize = function (bytes) {
        var sizes = ['bytes', 'kb', 'mb', 'gb', 'tb'];
        if (bytes == 1) return '1 <span>byte</span>';
-       if (bytes == 0) return '0';
+       if (bytes == 0) return '0 <span>bytes</span>';
        var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
        return Math.round(bytes / Math.pow(1024, i), 2) + ' <span>' + sizes[i] + '</span>';
     };
